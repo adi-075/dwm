abe7b6c8c81b14a87b5d245abf2b4b335750d878
adi-075 <adityamhambrey323@gmail.com>
2022-09-29 09:46:39 +0530

    Update Config

--- themes/red.h
+++ themes/red.h
@@ -2,6 +2,6 @@ static char normbgcolor[]           ="#1f2227";
 static char normbordercolor[]       ="#444444";
 static char normfgcolor[]           ="#eeeeee";
 static char selfgcolor[]            ="#FF0000";
-static char selbordercolor[]        ="#C0C0C0";
+static char selbordercolor[]        ="#FF0000";
 static char selbgcolor[]            ="#1f2227";
 
